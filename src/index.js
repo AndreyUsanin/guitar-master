b42a7c0e407d719c807e14d19d46b7f88dd12de9
rvwdude <rvwdude@gmail.com>
2018-10-22 11:30:05 +0300

    serious commit

--- src/index.js
+++ src/index.js
@@ -2,18 +2,5 @@ console.log('hello world');
 import './scss/main.scss';
 import './scss/fonts.css';
 
-import './js/a.js';
-
-
-import { tns } from "../node_modules/tiny-slider/src/tiny-slider";
-
-var slider = tns({
-  container: '.main-slider',
-  "items": 1,
-  "speed": 400,
-  controls: true,
-  "controlsContainer": ".main-slider__controls",
-  nav: false
-});
 
 
