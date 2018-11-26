4a80091958e7a7918b28fc807989b46829c0765e
rvwdude <rvwdude@gmail.com>
2018-11-26 08:32:20 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,4 +3,26 @@ import './scss/main.scss';
 import './scss/fonts.css';
 
 
+// baguetteBox GALLERY
+import baguetteBox from 'baguettebox.js';
 
+
+// NAV
+
+
+var toggle = document.querySelector(".toggle");
+var topNav = document.querySelector(".navi--top");
+
+
+// Listen for click event on toggle var
+toggle.addEventListener('click', function() {
+  console.log("toggle clicked");
+  // toggle class "active" on topNav var
+  topNav.classList.toggle("active");
+}, false);
+
+
+
+// GALLERY
+
+baguetteBox.run('.guitars');
